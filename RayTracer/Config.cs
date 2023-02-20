4431159d7e48403476a39e78746c33a78e4a0e58
Petr Kotáb <petrkotab99@gmail.com>
2023-02-20 22:06:44 +0100

    Added config file handling.

---
+++ RayTracer/Config.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace RayTracer;
+
+public class Config
+{
+    public int ImageWidth { get; set; }
+    public int ImageHeight { get; set; }
+    public string OutputFile { get; set; }
+
+    public static Config FromFile(string file)
+    {
+        if (!File.Exists(file)) 
+        {
+            Console.WriteLine($"Config file \"{file}\" does not exist!");
+            return null;
+        }
+
+        var serializer = new XmlSerializer(typeof(Config));
+        try
+        {
+            using var reader = new StreamReader(file);
+            var config = (Config)serializer.Deserialize(reader);
+            Logger.WriteLine("Config file loaded");
+            return config;
+        }
+        catch (Exception ex)
+        when(ex is IOException || ex is InvalidOperationException)
+        {
+            Console.WriteLine($"Error while loading config file: {ex.Message}.");
+            return null;
+        }
+    }
+}
