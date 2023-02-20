95a30c92ea02697f118608b0a896609ad2228132
Petr Kotáb <petrkotab99@gmail.com>
2023-02-20 19:37:41 +0100

    Added creating and saving the image.

--- RayTracer/Program.cs
+++ RayTracer/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
+using Utils;
+
+namespace RayTracer;
+
 class Program
 {
+    private const int width = 600;
+    private const int height = 450;
+    private const string outputFile = "demo.pfm";
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello world!");
+        var floatImage = new FloatImage(width, height, 3);
+        floatImage.SavePFM(outputFile);
+
+        Console.WriteLine("HDR image is finished.");
     }
 }
