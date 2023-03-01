f4981f97c97aaacdd6994d8872665c632439f310
Petr Kotáb <petrkotab99@gmail.com>
2023-03-01 18:03:48 +0100

    Added plane.

---
+++ RayTracer/Shapes/Plane.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK.Mathematics;
+
+
+namespace RayTracer.Shapes;
+class Plane : Shape
+{
+    private const double threshold = 1e-6;
+
+    public Vector3d Normal;
+    public Vector3d Position;
+
+    public override IntersectResult Intersect(Ray ray)
+    {
+        // P * N + d = 0
+
+        // t = - (P_0 * N + d) / (V * N)
+        // t = (S - P_0) * N / (P_1 * N)
+        double denominator = Vector3d.Dot(Normal, ray.Direction);
+
+        // no intersection
+        if (MathHelper.Abs(denominator) < threshold)
+            return IntersectResult.False;
+
+        double t = Vector3d.Dot(Position - ray.Position, Normal) / denominator;
+            
+        // point is in negative distance
+        if (t < 0)
+            return IntersectResult.False;
+            
+        return new IntersectResult(true, ray.Position + t * ray.Direction, t);
+    }
+}
