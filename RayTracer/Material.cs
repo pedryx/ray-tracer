1a88e9aced6ba8cbc1fc21484d5e1922ac1ff5a0
Petr Kotáb <petrkotab99@gmail.com>
2023-03-01 18:47:46 +0100

    Added materials.

---
+++ RayTracer/Material.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Mathematics;
+
+
+namespace RayTracer;
+/// <summary>
+/// Represent a material for Phong Shading Model.
+/// </summary>
+class Material
+{
+    /// <summary>
+    /// Ambient coeficient.
+    /// </summary>
+    public double Ambient;
+    /// <summary>
+    /// Diffuse coefficient.
+    /// </summary>
+    public double Diffuse;
+    /// <summary>
+    /// Specular Coefficient.
+    /// </summary>
+    public double Specular;
+    public double Highlight;
+    public Color4 Color;
+}
