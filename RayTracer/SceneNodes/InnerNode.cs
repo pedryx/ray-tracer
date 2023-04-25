526bfd97293f3833058da7cab9c4558ba78b88e3
Petr Kotáb <petrkotab99@gmail.com>
2023-04-25 19:26:35 +0200

    Added base classes for scene nodes.

---
+++ RayTracer/SceneNodes/InnerNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.SceneNodes;
+internal class InnerNode : ISceneNode
+{
+    public Matrix4x4 Transform;
+    public List<ISceneNode> Nodes = new();
+}
---
+++ RayTracer/SceneNodes/LeafNode.cs
@@ -0,0 +1,13 @@
+﻿using RayTracer.Shapes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer.SceneNodes;
+internal class LeafNode : ISceneNode
+{
+    public List<Shape> Shapes = new();
+}
